296cb507beb6ecf26c197592e5ea05ec88c59a17
Jannik Hollenbach <jannik@hollenbach.de>
2016-09-28 16:54:32 +0200

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row flex-items-xs-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-block">
+                    <h4 class="card-title">Dashboard</h4>
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <base href="{{ env('APP_URL', 'http://localhost/mealify/public') }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="css/app.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-white">
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2" aria-controls="exCollapsingNavbar2" aria-expanded="false" aria-label="Toggle navigation">
+            &#9776;
+        </button>
+        <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{-- config('app.name', 'Laravel') --}}
+                <img src="img/logo-with-text.svg" height="50" alt="">
+            </a>
+            <ul class="nav navbar-nav pull-xs-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="{{ url('/logout') }}"
+                               class="dropdown-item"
+                               onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </li>
+                @endif
+            </ul>
+            <ul class="nav navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Recipes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Meals</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Recipe Finder</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Your Recipes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Your Storage</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
+
+    <!-- Scripts -->
+    <script src="js/app.js"></script>
+</body>
+</html>
