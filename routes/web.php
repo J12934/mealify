82dc1eab5484a4d7970b9bf10bed94052f453153
J12934 <jannik@hollenbach.de>
2016-09-28 17:58:00 +0200

    Reworked Login Routes and Infrastructure

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ExploreController@index');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
