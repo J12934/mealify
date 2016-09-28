e59b1507d3ae1e424c29e23990bfefe8044440a2
J12934 <jannik@hollenbach.de>
2016-09-28 19:50:01 +0200

    Minor Explore and Card Tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="card-columns">
-        @foreach($recipes as $recipe)
-            <div class="card card-media">
-                <img class="card-img w-100" src="{{ $recipe->image }}" alt="Card image cap">
-                <div class="card-img-overlay flex flex-items-xs-bottom">
-                    <h4 class="card-title text-white flex">{{ $recipe->name }}</h4>
-                </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h2>Discover new Recepies</h2>
+        </div>
+        <div class="col-lg-12">
+            <div class="card-columns">
+                @foreach($recipes as $recipe)
+                    <div class="card card-media">
+                        <img class="card-img w-100" src="{{ $recipe->image }}" alt="Card image cap">
+                        <div class="card-img-overlay flex flex-items-xs-bottom">
+                            <h4 class="card-title text-white flex">{{ $recipe->name }}</h4>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </div>
+
 </div>
 @endsection
